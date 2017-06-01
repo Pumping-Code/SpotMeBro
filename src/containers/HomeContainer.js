864f596447b46672df07caa7601c76fe31840456
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-05-31 20:43:53 -0500

    cleaned up code for geolocaiton

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -5,51 +5,51 @@ import { Constants, Location, Permissions } from 'expo';
 import styles from '../styles/styles';
 
 class HomeContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			location: null,
-			errorMessage: null,
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: null,
+      errorMessage: null,
+    };
+  }
 
   componentWillMount() {
-	 if (Platform.OS === 'android' && !Constants.isDevice) {
-		 this.setState({
-			 errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-		 });
-	 } else {
-		 this._getLocationAsync();
-	 }
- };
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this.getLocationAsync();
+    }
+  }
 
-	_getLocationAsync = async () => {
-	 let { status } = await Permissions.askAsync(Permissions.LOCATION);
-	 if (status !== 'granted') {
-		 this.setState({
-			 errorMessage: 'Permission to access location was denied',
-		 });
-	 }
+  async getLocationAsync() {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
 
-	 await Location.watchPositionAsync({
-		 distanceInterval: 1,
-	 },(location) => {
-		 this.setState({ location });
-	 });
- };
+    await Location.watchPositionAsync({
+      distanceInterval: 1,
+    }, (location) => {
+      this.setState({ location });
+    });
+  }
 
   render() {
-		let text = 'Waiting..';
-		if (this.state.errorMessage) {
-			text = this.state.errorMessage;
-		} else if (this.state.location) {
-			text = JSON.stringify(this.state.location);
-		}
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
 
     return (
       <View style={styles.container}>
         <Text>Spot Me Bro</Text>
-				<Text>{text}</Text>
+        <Text>{text}</Text>
       </View>
     );
   }
