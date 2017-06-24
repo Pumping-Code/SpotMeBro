177f1e9ade2efd09f4a6a6a042501ac3a041833d
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-06-23 21:29:03 -0500

    merged in master

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,71 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Platform, View, Text } from 'react-native';
-import { Constants, Location, Permissions } from 'expo';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
+import Home from '../components/home/Home';
 import styles from '../styles/styles';
 
-class HomeContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: null,
-      errorMessage: null,
-    };
-  }
-
-  componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this.getLocationAsync();
-    }
-  }
-
-  async getLocationAsync() {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
-
-    await Location.watchPositionAsync({
-      distanceInterval: 1,
-    }, (location) => {
-      this.setState({ location });
-    });
-  }
-
-  render() {
-    let text = 'Waiting..';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.location) {
-      text = JSON.stringify(this.state.location);
-    }
-
-    return (
-      <Container>
-        <Content>
-          <Text>{text}</Text>
-          <Button>
-            <Text>I Need A Spot</Text>
-          </Button>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
-  }
-}
+const HomeContainer = props => (
+  <Home {...props} />
+);
 
 const mapStateToProps = state => state;
 
